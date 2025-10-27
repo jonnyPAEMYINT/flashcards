0c7d96aa92c8bb6e8f822ceb8aeaddaae8d49919
jonnyPAEMYINT <jonathanpaemyint@gmail.com>
2025-10-27 23:00:08 +0630

    Update main.js

--- js/main.js
+++ js/main.js
@@ -12,10 +12,9 @@ document.addEventListener('DOMContentLoaded', () => {
   let currentIndex = 0;
   let flipped = false;
 
-  async function loadCards(file = 'flashcards/data/flashcards_phrases.json') {
+  async function loadCards(file = 'https://jonnypaemyint.github.io/flashcards/data/flashcards_phrases.json') {
     flashcard.textContent = 'Loading...';
     try {
-      console.log(file);
       const res = await fetch(file);
       cards = await res.json();
       resetDeckOriginal();
